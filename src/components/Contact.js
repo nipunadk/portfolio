151e10dfea634fc33815287fff6875f9c7fb15aa
nipunadk <nipunadk@gmail.com>
2023-06-11 09:42:52 +0530

    Added twiiter handle

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,12 @@
 import React, { useRef } from "react";
 import mobileIcon from "../public/img/mobileIcon.png";
 import emailIcon from "../public/img/mailIco.png";
+import twitterIcon from "../public/img/twitter.png";
 import emailjs from "@emailjs/browser";
 
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-toast.configure(); 
+toast.configure();
 
 export default function Contact() {
   const form = useRef();
@@ -26,11 +27,11 @@ export default function Contact() {
           var textArea = document.querySelectorAll('textarea');
           textArea.forEach(singleInput => singleInput.value = '');
           toast('Your email has been sent !')
-          
+
         },
         (error) => {
           toast.error('Something went wrong :( Please use LinkedIn/mobile to reach')
-          
+
         }
       );
   };
@@ -52,26 +53,38 @@ export default function Contact() {
             />
             <textarea
               placeholder="Type your contact and message"
-              rows={10}
+              rows={5}
               name="message"
             />
             <button type="submit" value="Send">
               Send Email
             </button>
-            
+
           </form>
           <div className="contact">
             <span>
-              <img src={mobileIcon} alt="mobile" /> +94 77 24 22 777
-            </span>
-            <span>
-              <img src={emailIcon}  alt="email"></img>
+              <img src={emailIcon} alt="email"></img>
               <a
                 href="mailto:dkcngomes@gmail.com"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 dkcngomes@gmail.com
+              </a>
+            </span>
+            <span>
+              <img src={mobileIcon} alt="mobile" /> +94 77 24 22 777
+            </span>
+            <span>
+              <img
+                src={twitterIcon}
+                alt="twitter"
+              />
+              <a href="https://twitter.com/dkcnipuna"
+                target="_blank"
+                rel="noreferrer" 
+              > 
+                https://twitter.com/dkcnipuna
               </a>
             </span>
           </div>
